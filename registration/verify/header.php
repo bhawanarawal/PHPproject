7febbe09b7aaaddcf99c9e900014aa8e2740f14b
Your Name <you@example.com>
2024-11-18 12:49:19 +0545

    add registration

---
+++ registration/verify/header.php
@@ -0,0 +1,6 @@
+<div>
+    <a href="index.php">Home</a>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Signup</a>
+    <a href="profile.php">profile</a>
+</div>
---
+++ registration/verify/signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+    <h1>Signup</h1>
+
+    <?php include('header.php')?>
+
+    <div>
+        <form>
+            <input type="text" name="username" place
+        </form>
+    </div>
+
+</body>
+</html>
